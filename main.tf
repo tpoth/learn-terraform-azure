479e25cb48dcdaae5ca7a0d072985b1ea383ce64
Thomas Poth (TMK) <thomas.poth@zv-extern.fraunhofer.de>
2022-04-12 15:54:07 +0200

    add a Vnet with range 10.0.0.0/16

--- main.tf
+++ main.tf
@@ -18,3 +18,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResoucreGroup"
   location = "West Europe"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name = "myTFVnet"
+  address_space = ["10.0.0.0/16"]
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
