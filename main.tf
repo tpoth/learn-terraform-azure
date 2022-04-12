2c29d1a12345988433948115288837921fa627b3
Thomas Poth (TMK) <thomas.poth@zv-extern.fraunhofer.de>
2022-04-12 16:09:09 +0200

    add variable for the rg

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResoucreGroup"
+  name     = var.resource_group_name
   location = "West Europe"
 
   tags = {
