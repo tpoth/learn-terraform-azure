c597be63eaaf213fd427d27588f6cb8b4efa3095
Thomas Poth (TMK) <thomas.poth@zv-extern.fraunhofer.de>
2022-04-12 16:53:58 +0200

    move state to terraform cloud

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-
-  required_version = ">= 1.1.0"
+  cloud {
+      organization = "pubt"
+      workspaces {
+          name = "learn-terraform-azure"
+      }
+  }
 }
 
 provider "azurerm" {
